2c8772acaeb15a68dbab7c3a45f9a227e460a101
sujit kori <sujitkori12@gmail.com>
2022-10-13 21:09:58 +0530

    This is a component.js component made by sujit

---
+++ src/components/Component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Component = () => {
+  return (
+    <div>
+      This is a Component and i want to add this in my local branch of my Git.......
+    </div>
+  )
+}
+
+export default Component
